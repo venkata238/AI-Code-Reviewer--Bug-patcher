a906d77338867ca0f39bd63349be0b7b96394268
S.V Karthigeyan <svkarthigeyan@gmail.com>
2026-05-18 22:58:00 +0530

    Delete server directory

--- server/routes/webhook.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const webhookController = require('../controllers/webhookController');
-const { verifyGithubSignature } = require('../middleware/verifyGithubSignature');
-
-router.post('/', verifyGithubSignature, webhookController.handleGithubWebhook);
-
-module.exports = router;
