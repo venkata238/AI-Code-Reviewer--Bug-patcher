e398819b76a029569a62643531a1a9e22f1b48ad
lalasa496 <arangilalasa@gmail.com>
2026-05-21 16:10:50 +0530

    Added GitHub PR file fetching logic

--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const webhookController = require('../controllers/webhookController');
 const { verifyGithubSignature } = require('../middleware/verifyGithubSignature');
-
 router.post('/', verifyGithubSignature, webhookController.handleGithubWebhook);
 
 module.exports = router;
